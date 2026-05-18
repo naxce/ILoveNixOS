c3cbd5443e235131cf9853faca460b7ac4158fe9
naxce <naxcehelltea@gmail.com>
2026-05-19 00:50:47 +0200

    Update Commit

--- Modules/system/boot.nix
+++ Modules/system/boot.nix
@@ -6,6 +6,7 @@
 }:
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
