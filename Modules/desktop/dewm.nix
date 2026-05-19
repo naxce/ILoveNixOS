b1348eb74f0d24fd00eaafde00fac93905e0c177
naxce <naxcehelltea@gmail.com>
2026-05-19 20:20:12 +0200

    Update Commit

--- Modules/desktop/dewm.nix
+++ Modules/desktop/dewm.nix
@@ -13,6 +13,13 @@
   services.desktopManager.plasma6.enable = true;
   environment.variables.BALOO_ENABLED = "0";
 
+  # Maliit
+  environment.sessionVariables = {
+    QT_IM_MODULE = "maliit";
+    GTK_IM_MODULE = "maliit";
+    XMODIFIERS = "@im=maliit";
+  };
+
   # KDE Plasma Debloat
   services.printing.enable = false;
   services.avahi.enable = false;
