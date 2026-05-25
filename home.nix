5804d61b349262611524e802795950fd5c325195
naxce <naxcehelltea@gmail.com>
2026-05-25 21:14:27 +0200

    Update Commit

--- home.nix
+++ home.nix
@@ -46,6 +46,12 @@
   home.file.".config/kitty".source = ./Config/kitty;
   home.file.".config/sptlrx".source = ./Config/sptlrx;
 
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    GTK_IM_MODULE = "wayland";
+    QT_IM_MODULE = "qtvirtualkeyboard";
+  };
+
   programs.bash = {
     enable = true;
 
