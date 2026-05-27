17694fa999390648738c851c47fe45618cb30c37
naxce <naxcehelltea@gmail.com>
2026-05-28 00:21:44 +0200

    Update Commit

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,6 @@
     ./Modules/desktop/gaming.nix
     ./Modules/desktop/packages.nix
 
-    ./Modules/desktop/waybar/waybar.nix
-
     ./Modules/system/boot.nix
     ./Modules/system/hardware.nix
     ./Modules/system/mount.nix
