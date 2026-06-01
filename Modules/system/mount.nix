290c7bd80c7c056371587fd95cfa8353cbf3dc45
naxce <naxcehelltea@gmail.com>
2026-06-01 14:54:10 +0200

    Update Commit

--- Modules/system/mount.nix
+++ Modules/system/mount.nix
@@ -6,10 +6,10 @@
 }:
 
 {
-boot.supportedFilesystems = [ "ntfs" ];
+  boot.supportedFilesystems = [ "ntfs3" ];
   fileSystems."/mnt/windows" = {
     device = "/dev/disk/by-uuid/186AAA106AA9EAA8";
-    fsType = "ntfs-3g";
+    fsType = "ntfs3";
     options = [
       "rw"
       "uid=1000"
@@ -18,12 +18,13 @@ boot.supportedFilesystems = [ "ntfs" ];
   };
   fileSystems."/mnt/data" = {
     device = "/dev/disk/by-uuid/3B3A1DA152B204A4";
-    fsType = "ntfs-3g";
+    fsType = "ntfs3";
     options = [
       "rw"
       "uid=1000"
-      "gid=100"
-      "umask=002"
+      "gid=1000"
+      "umask=022"
+      "big_writes"
       "nofail"
     ];
   };
