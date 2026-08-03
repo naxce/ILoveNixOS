0795db5f7bed446c412730cb707109efa12f2a37
naxce <naxcehelltea@gmail.com>
2026-08-03 16:01:16 +0200

    Update Commit

--- Modules/system/mount.nix
+++ Modules/system/mount.nix
@@ -31,14 +31,4 @@
       "x-systemd.device-timeout=10"
     ];
   };
-  fileSystems."/mnt/void" = {
-    device = "/dev/disk/by-uuid/5f0be456-4e60-4018-9de9-61df53981518";
-    fsType = "ext4";
-    options = [
-      "rw"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.device-timeout=10"
-    ];
-  };
 }
