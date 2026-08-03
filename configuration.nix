4597834788760c70f63833f3856c6f731996260b
naxce <naxcehelltea@gmail.com>
2026-08-03 15:47:47 +0200

    Update Commit

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,6 @@
     ./Modules/desktop/gaming.nix
     ./Modules/desktop/packages.nix
     ./Modules/desktop/control-center.nix
-    ./Modules/desktop/hyprlogin.nix
 
     ./Modules/system/boot.nix
     ./Modules/system/hardware.nix
