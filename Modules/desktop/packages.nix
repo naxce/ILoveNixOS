4dd12e0cf99d9b9463cec87b05fe02dc22d3aa0a
naxce <naxcehelltea@gmail.com>
2026-09-23 00:52:18 +0200

    Update Commit

--- Modules/desktop/packages.nix
+++ Modules/desktop/packages.nix
@@ -15,7 +15,7 @@ let
         platforms = {
           "linux-x64" = {
             binary = "claude.zst";
-            checksum = sha256-J5EOKucE2PLoAkiX2P3x53EIB7r09pgsDjeXwFgxU4Q=;
+            checksum = "sha256-J5EOKucE2PLoAkiX2P3x53EIB7r09pgsDjeXwFgxU4Q=";
           };
         };
       };
